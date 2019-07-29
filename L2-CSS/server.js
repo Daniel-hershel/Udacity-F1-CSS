d7440d55023e99aaf52bd48d7fb97a6f93a184cc
hershel <daniel.h.silber.baker@gmail.com>
2019-07-29 11:34:44 -0700

    up to the cloud

---
+++ L2-CSS/server.js
@@ -0,0 +1,27 @@
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 3000;
+// Spin up the server
+const server = app.listen(port, listening);
+// Callback to debug
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
